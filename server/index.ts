e0ea3d5e3126e721d8e77b860c848f3253732086
Asaiy420 <kcaryan05@gmail.com>
2025-07-17 18:58:43 +0545

    refactor: enhance user management and message handling in websocket server

--- server/index.ts
+++ server/index.ts
@@ -2,6 +2,12 @@ import http from "http";
 import { WebSocketServer, WebSocket } from "ws";
 import url from "url";
 import { v4 as uuidv4 } from "uuid";
+
+type User = {
+  username: string | string[] | undefined;
+  state: Object;
+};
+
 const server = http.createServer();
 
 const wss = new WebSocketServer({ server });
@@ -9,7 +15,33 @@ const wss = new WebSocketServer({ server });
 const PORT = 8000;
 
 const connections: { [key: string]: WebSocket } = {};
-const users: {[key: string]: Object} = {};
+
+const users: { [key: string]: User } = {};
+
+const broadcast = () => {
+  Object.keys(connections).forEach((uuid) => {
+    const connection = connections[uuid];
+    const message = JSON.stringify(users);
+    connection?.send(message);
+  });
+};
+
+const handleMessage = (bytes: WebSocket.RawData, uuid: string) => {
+  const message = JSON.parse(bytes.toString());
+  const user = users[uuid];
+  if (!user) return;
+  user.state = message;
+
+  broadcast();
+};
+
+const handleClose = (uuid: string) => {
+  delete connections[uuid];
+  delete users[uuid]
+
+  broadcast();
+
+};
 
 wss.on("connection", (connection, request: http.IncomingMessage) => {
   const requestUrl = request.url || "";
@@ -19,15 +51,13 @@ wss.on("connection", (connection, request: http.IncomingMessage) => {
 
   connections[uuid] = connection;
 
-
   users[uuid] = {
     username,
-    state: { // basically keeping track of what the user is doing 
-      x: 0,
-      y: 0,
-    }
-  }
+    state: {},
+  };
 
+  connection.on("message", (message) => handleMessage(message, uuid));
+  connection.on("close", () => handleClose(uuid));
 });
 
 server.listen(PORT, () => {
